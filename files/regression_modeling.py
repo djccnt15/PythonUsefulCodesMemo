64fab14d038763143dabbe7bf922a1a939111931
djccnt15 <53306911+djccnt15@users.noreply.github.com>
2021-05-04 16:39:27 +0900

    Update regression_modeling.py

--- files/regression_modeling.py
+++ files/regression_modeling.py
@@ -1,18 +1,14 @@
+import pandas as pd
+import statsmodels.formula.api as smf
+
+dataset = pd.read_csv(dataset.csv)
+
 '''targetting var'''
-# independ var
-names = train.columns[:]
-independ_names = ""
-
-for i in names:
-    if independ_names == "": independ_names = i
-    else: independ_names = independ_names + " + " + i
-# print(independ_names)
-
-# depend var
-depend_name = "EF07ExpectMatuLevel"
+independ_names = ' + '.join(dataset.columns)
+depend_name = "target"
 
 '''modeling'''
-model = depend_name + " ~ " + independ_names
+model = depend_name + ' ~ ' + independ_names
 # print(model)
 
-result = sm.ols(formula=model, data=train)
+result = smf.ols(formula=model, data=dataset)
