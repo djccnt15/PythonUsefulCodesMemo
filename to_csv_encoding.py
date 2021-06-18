67fdd4c750776f4e15b587b865dc9d622f2e2312
djccnt15 <djccnt15@gmail.com>
2021-06-18 14:07:45 +0900

    improve to_csv_encoding.py

--- to_csv_encoding.py
+++ to_csv_encoding.py
@@ -1,9 +1,15 @@
+import os
 import pandas as pd
 
-a = '2013 소상공인 실태조사'
-b = '2014 소상공인 실태조사'
-data_list = [a, b]
-df = pd.DataFrame(data_list)
-print(df)
+'''configuration'''
+file_dir = 'C:\\projects\\scraping_twitter\\twitter_scrap\\gov_smartfactory'
 
-df.to_csv('./sample.csv', encoding='utf-8-sig')
+def encoding(file):
+    df = pd.read_csv(filepath_or_buffer=file)
+    df.to_csv('./encoded_%s' %file, encoding='utf-8-sig')
+
+os.chdir(file_dir)
+file_list = os.listdir()
+
+for f in os.listdir():
+    encoding(f)
